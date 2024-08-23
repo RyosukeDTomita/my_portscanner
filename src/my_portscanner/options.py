22a978177a7b792b5fb56b8e91d7305b0e218ed0
RyosukeDTomita <d310151c2@gmail.com>
2024-08-24 03:14:39 +0900

    Return ValueError for invalid port number

--- src/my_portscanner/options.py
+++ src/my_portscanner/options.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import argparse
+import sys
+from typing import Union
 from .version import __version__
 
 
@@ -34,7 +36,11 @@ def parse_args() -> dict:
     parser.add_argument("--version", action="version", version=__version__)
     p = parser.parse_args()
 
-    port_list = _create_port_list(p.port)
+    try:
+        port_list = _create_port_list(p.port)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
 
     if p.stealth_scan:
         scan_type = "stealth"
@@ -55,24 +61,35 @@ def parse_args() -> dict:
     return args
 
 
-def _create_port_list(port: str) -> list[int]:
+def _create_port_list(port: Union[str, None]) -> list[int]:
     """_create_port_list.
     -p オプションの引数をリストに変換する
 
     Args:
-        port (str): port number lists. port number range e.g: -p 22,80,443 -p 22-30
+        port str: port number lists. port number range e.g: -p 22,80,443 -p 22-30
+        port None: no -p option
 
     Returns:
         list[int]: e.g: [22, 80, 443]
     """
+    default_port_list = [22, 80, 443]
+
+    if port is None:
+        return default_port_list
+    if port.isdigit():
+        return [int(port)]
+
+    # ,区切りをリストに変換
     if "," in port:
-        port_list = [int(x) for x in port.split(",")]
+        port_list = [int(x) for x in port.split(",") if x.isdigit()]
+        if port_list == []:
+            raise ValueError("[-p] port list includes non-digit")
+    # -のport範囲指定をリストに変換
     elif "-" in port:
-        port_list = [
-            int(x)
-            for x in range(int(port.split("-")[0]), int(port.split("-")[1]) + 1, 1)
-        ]
-    else:
-        # default port list
-        port_list = [22, 80, 443]
+        port_start_str, port_end_str = port.split("-")
+        if (not port_start_str.isdigit()) or (not port_end_str.isdigit()):
+            raise ValueError("[-p] port range includes non-digit")
+        if int(port_start_str) > int(port_end_str):
+            raise ValueError("[-p] port range start is larger than end")
+        port_list = [int(x) for x in range(int(port_start_str), int(port_end_str) + 1)]
     return port_list
